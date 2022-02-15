c1ab5c5f3deccd342736fcb7cea210d82388c6e0
Fad F <90547391+FadTheChad@users.noreply.github.com>
2022-02-15 23:50:08 +0500

    Changed script name

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import yargs from 'yargs'
 yargs.version('1.0.0')
 
 yargs
-    .scriptName("pirate-parser")
+    .scriptName("bolt")
     .usage('$0 <cmd> [args]')
     .commandDir('commands')
     .demandCommand()
