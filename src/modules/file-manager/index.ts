25fc95a79b6ed83c11907c508abde265a37391ae
Fad F <fmzfads@gmail.com>
2022-02-19 00:41:31 +0500

    added a FileManager class for handling file creation, editing, etc

--- src/modules/file-manager/index.ts
+++ src/modules/file-manager/index.ts
@@ -1,6 +1,33 @@
+import fs from 'fs'
+import path from 'path'
+
 class FileManager {
-    public createDir(dirPath: string) {
-        
+    public createDir(rootPath: string, dirName: string) {
+        const dirPath = path.join(rootPath, dirName)
+
+        if (fs.existsSync(dirPath)) throw new Error('Dir already exists')
+
+        fs.mkdirSync(dirPath)
+    }
+
+    public editFile(filePath: string, fileContentArray: string[]) {
+        if (!fs.existsSync(filePath)) throw new Error('Dir does not exist')
+
+        fs.writeFileSync(filePath, fileContentArray.join('\n'))
+    }
+
+    public createFile(rootPath: string, fileName: string, fileContentArray: string[] = []) {
+        const filePath = path.join(rootPath, fileName)
+
+        if (fs.existsSync(filePath)) throw new Error('Dir already exists')
+
+        fs.writeFileSync(filePath, '')
+
+        this.editFile(filePath, fileContentArray)
+    }
+
+    public initProject() {
+        console.log(process.cwd())
     }
 }
 
