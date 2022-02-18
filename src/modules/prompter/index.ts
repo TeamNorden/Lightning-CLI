632f99a41c487bda3ca3f2ab1d5d4eb8bfc10d17
Fad F <fmzfads@gmail.com>
2022-02-19 00:40:34 +0500

    removed options constructor from prompter class and moved it to a specific func

--- src/modules/prompter/index.ts
+++ src/modules/prompter/index.ts
@@ -2,13 +2,7 @@ import prompts from 'prompts'
 import { string } from 'yargs'
 
 class Prompter<TOptionNames extends string = string> {
-    public options: prompts.PromptObject<TOptionNames>[]
-
     public config = new Map<TOptionNames, unknown>()
-
-    constructor(options: prompts.PromptObject<TOptionNames>[]) {
-        this.options = options
-    }
 
     public async input<T extends string = string>(questions: prompts.PromptObject<T>) {
         const res = await prompts<T>(questions)
@@ -16,8 +10,8 @@ class Prompter<TOptionNames extends string = string> {
         return res
     }
 
-    public async init() {
-        for (const option of this.options) {
+    public async init(options: prompts.PromptObject<TOptionNames>[]) {
+        for (const option of options) {
             const answer = await this.input(option)
 
             const optionName = (typeof option.name === 'string' ? option.name : Object.keys(answer)[0]) as TOptionNames
