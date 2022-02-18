84be1808034ab14d5874f7c32b665a3c8d183faf
Fad F <fmzfads@gmail.com>
2022-02-19 00:40:02 +0500

    client is exported and created in main file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import yargs from 'yargs'
+import BoltCLIClient from './modules/client'
+import Prompter from './modules/prompter'
+import FileManager from './modules/file-manager'
+
+export const client = new BoltCLIClient(FileManager, Prompter)
 
 yargs.version('0.1.0')
 
