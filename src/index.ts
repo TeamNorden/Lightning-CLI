e4e72bbe6701ac9353a91a68d336be6f08fe9844
Fad F <fmzfads@gmail.com>
2022-02-23 13:17:41 +0500

    cli is now built in the client's build method

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,6 @@ import FileManager from './modules/file-manager'
 
 export const client = new BoltCLIClient(FileManager, Prompter)
 
-yargs.version('0.1.0')
-
-yargs
-    .scriptName("bolt")
-    .usage('$0 <cmd> [args]')
-    .commandDir('commands')
-    .demandCommand()
-    .help()
-    .argv
+;(async () => {
+    (await client.build()).argv
+})()
--- src/modules/client/index.ts
+++ src/modules/client/index.ts
@@ -1,18 +1,41 @@
 import FileManager from '../file-manager'
 import Prompter from '../prompter'
 import { Class } from 'type-fest'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+import { readdirSync } from 'fs'
+import path from 'path'
+import ConfigMap from '../config-map'
 
 class BoltCLIClient {
     public fileManager: FileManager
     public prompter: Prompter
 
     constructor(FileManagerClass: Class<FileManager>, PrompterClass: Class<Prompter>) {
-        this.fileManager = new FileManagerClass()
+        this.fileManager = new FileManagerClass(new ConfigMap())
         this.prompter = new PrompterClass()
     }
 
-    start() {
-        
+    async build() {
+        const cli = yargs(hideBin(process.argv)).scriptName('bolt')
+
+        for (const commandFile of readdirSync(path.join(__dirname, '../../commands'))
+                .filter(f => f.endsWith('.js'))
+            ) {
+            const command = await import(path.join(__dirname, '../../commands', commandFile))
+
+            yargs.command(command)
+        }
+
+        const pkgJson = '../../../package.json'
+
+        cli
+            .version((await import(pkgJson)).version)
+            .help()
+            .demandCommand()
+            .usage('$0 <cmd> [args]')
+
+        return cli
     }
 }
 
