ca6346814e04b2849d48729d307956cc371afdf2
Fad F <fmzfads@gmail.com>
2022-02-24 14:43:27 +0500

    turned all FileManager functions async to not fuck stuff up

--- src/modules/file-manager/index.ts
+++ src/modules/file-manager/index.ts
@@ -1,4 +1,4 @@
-import fs from 'fs'
+import { promises as fs } from 'fs'
 import path from 'path'
 import ConfigMap from '../config-map'
 
@@ -8,33 +8,63 @@ class FileManager {
     constructor(config: ConfigMap) {
         this.config = config
     }
-
-    public createDir(rootPath: string, dirName: string) {
+    
+    public async pathExists(path: string) {
+        try {
+            await fs.access(path)
+            return true
+        } catch (e) {
+            return false
+        }
+    }
+    
+    public async createDir(rootPath: string, dirName: string) {
         const dirPath = path.join(rootPath, dirName)
 
-        if (fs.existsSync(dirPath)) throw new Error('Dir already exists')
+        if (await this.pathExists(dirPath)) throw new Error('Dir already exists')
 
-        fs.mkdirSync(dirPath)
+        await fs.mkdir(dirPath)
+        return dirPath
     }
 
-    public editFile(filePath: string, fileContentArray: string[]) {
-        if (!fs.existsSync(filePath)) throw new Error('Dir does not exist')
+    public async editFile(filePath: string, fileContentArray: string[]) {
+        if (!(await this.pathExists(filePath))) throw new Error('Dir does not exist')
 
-        fs.writeFileSync(filePath, fileContentArray.join('\n'))
+        await fs.writeFile(filePath, fileContentArray.join('\n'))
+        return filePath
     }
 
-    public createFile(rootPath: string, fileName: string, fileContentArray: string[] = []) {
+    public async createFile(rootPath: string, fileName: string, fileContentArray: string[] = []) {
         const filePath = path.join(rootPath, fileName)
 
-        if (fs.existsSync(filePath)) throw new Error('Dir already exists')
+        if (await this.pathExists(filePath)) throw new Error('Dir already exists')
 
-        fs.writeFileSync(filePath, '')
+        await fs.writeFile(filePath, '')
 
-        this.editFile(filePath, fileContentArray)
+        await this.editFile(filePath, fileContentArray)
+        return filePath
     }
 
-    public initProject() {
-        console.log(process.cwd())
+    public async initProject() {
+        console.log('Creating Project...\n\n')
+
+        const projectPath = this.config.get('project') as string
+
+        if (!(await this.pathExists(projectPath))) await this.createDir(projectPath, '.')
+
+        console.log('Creating Source Folder...')
+
+        const srcPath = await this.createDir(projectPath, 'src')
+
+        console.log('\tSource Folder Created!\n')
+
+        console.log('Creating Other Project Folders/Files...')
+
+        await this.createFile(
+            srcPath,
+            'index.' + this.config.get('language')!,
+            ['console.log("Hello World!")']
+        )
     }
 }
 
