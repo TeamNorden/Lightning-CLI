e06fb5449dadaf74c249bf704500ca02571a714b
Fad F <fmzfads@gmail.com>
2022-02-24 14:42:55 +0500

    function returned typings dont work properly so gonna fix that later

--- src/modules/config-map/index.ts
+++ src/modules/config-map/index.ts
@@ -4,4 +4,5 @@ export interface IConfig {
     pkgManager: 'npm' | 'yarn'
 }
 
+// Doesnt really have full typings. Will be worked on later
 export default class ConfigMap<K extends keyof IConfig = keyof IConfig, V = IConfig[K]> extends Map<K, V> { }
