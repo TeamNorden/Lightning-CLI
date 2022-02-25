65f0c5ef13841d4745c03eaa813a6e2a513580a9
Fad F <fmzfads@gmail.com>
2022-02-25 23:41:01 +0500

    allowed edit and create functions to accept content strings, and added more file creation

--- src/modules/file-manager/index.ts
+++ src/modules/file-manager/index.ts
@@ -1,6 +1,7 @@
 import { promises as fs } from 'fs'
 import path from 'path'
 import ConfigMap from '../config-map'
+import { exec, execSync } from 'child_process'
 
 class FileManager {
     public config = new ConfigMap()
@@ -27,44 +28,75 @@ class FileManager {
         return dirPath
     }
 
-    public async editFile(filePath: string, fileContentArray: string[]) {
+    public async editFile(filePath: string, fileContent: string | string[]) {
         if (!(await this.pathExists(filePath))) throw new Error('Dir does not exist')
 
-        await fs.writeFile(filePath, fileContentArray.join('\n'))
+        await fs.writeFile(filePath, Array.isArray(fileContent) ? fileContent.join('\n') : fileContent)
         return filePath
     }
 
-    public async createFile(rootPath: string, fileName: string, fileContentArray: string[] = []) {
+    public async createFile(rootPath: string, fileName: string, fileContent: string | string[] = '') {
         const filePath = path.join(rootPath, fileName)
 
         if (await this.pathExists(filePath)) throw new Error('Dir already exists')
 
         await fs.writeFile(filePath, '')
 
-        await this.editFile(filePath, fileContentArray)
+        await this.editFile(filePath, fileContent)
         return filePath
     }
 
     public async initProject() {
-        console.log('Creating Project...\n\n')
+        console.log('\nCreating Project...')
 
         const projectPath = this.config.get('project') as string
 
         if (!(await this.pathExists(projectPath))) await this.createDir(projectPath, '.')
 
-        console.log('Creating Source Folder...')
+        console.log('\nCreating Source Folder...')
 
         const srcPath = await this.createDir(projectPath, 'src')
 
-        console.log('\tSource Folder Created!\n')
+        console.log('\tSource Folder Created!')
 
-        console.log('Creating Other Project Folders/Files...')
+        console.log('\nCreating Other Project Folders/Files...')
 
         await this.createFile(
             srcPath,
             'index.' + this.config.get('language')!,
             ['console.log("Hello World!")']
         )
+
+        console.log('\tIndex File Created!')
+
+        const pkgJsonTemplatePath = '../../../templates/package-template.json'
+
+        const pkgJsonTemplate = (await import(pkgJsonTemplatePath)).default
+
+        if (this.config.get('language') === 'js') {
+            pkgJsonTemplate.main = 'src/index.js'
+            pkgJsonTemplate.scripts.start = 'node --experimental-specifier-resolution=node .'
+        }
+
+        await this.createFile(srcPath, 'package.json', JSON.stringify(pkgJsonTemplate, null, 2))
+
+        console.log('\tPackage File Created!')
+
+        if (this.config.get('language') === 'ts') {
+            const tsConfigTemplatePath = '../../../templates/tsconfig-template.json'
+
+            const tsConfigTemplate = (await import(tsConfigTemplatePath)).default
+
+            await this.createFile(srcPath, 'tsconfig.json', JSON.stringify(tsConfigTemplate, null, 2))
+
+            console.log('\t[TS] TSConfig File Created!')
+        }
+
+        const pkgMn = this.config.get('pkgManager')!
+
+        const cmd = pkgMn === 'npm' ? pkgMn + ' i' : pkgMn
+
+        console.log('\nDone! Now you can start by installing the dependencies with `' + cmd + '`')
     }
 }
 
